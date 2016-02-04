b780cdd5acffc926a39d578e12c15b85a2c10591
沈辰 <changzheng.ycz@alibaba-inc.com>
2016-02-04 11:08:22 +0800

    initial commit

---
+++ cpp/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  cpp
+//
+//  Created by   linkary on 2/3/16.
+//  Copyright © 2016 linkary. All rights reserved.
+//
+
+#include "genlib.h"
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+/* Main program */
+int main(int argc, const char * argv[]) {
+	cout << "hello world!" << endl;
+
+    return 0;
+}
